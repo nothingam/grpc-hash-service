e06c95620efa795e7fee35529bd70830eeff8736
Peter Olah <p.nothingam@gmail.com>
2024-06-10 16:15:08 +0200

    minor changes in logging

--- client.py
+++ client.py
@@ -2,21 +2,26 @@ import grpc
 import hash_pb2
 import hash_pb2_grpc
 import os
-import random
+import logging
 
 from config import FE_SERVER_CERT
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 def generate_requests(num_requests, size):
     for _ in range(num_requests):
         data = os.urandom(size)
+        logging.info(f'Sending request of size {len(data)} bytes.')
         yield hash_pb2.HashRequest(data=data)
 
 def run():
     with grpc.secure_channel('localhost:50052', grpc.ssl_channel_credentials(root_certificates=FE_SERVER_CERT)) as channel:
         stub = hash_pb2_grpc.HasherStub(channel)
-        responses = stub.HashMe(generate_requests(100, 32768*2))
-        for response in responses:
-            print(f"Received hash: {response.hash.hex()}")
+        for response in stub.HashMe(generate_requests(100, 1024*1024)):
+            logging.info(f"Recieved hash: {response.hash.hex()}") 
 
 if __name__ == '__main__':
     run()
--- server.py
+++ server.py
@@ -4,12 +4,19 @@ import hashlib
 import hash_pb2
 import hash_pb2_grpc
 from config import SERVER_CERT, SERVER_KEY
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 class HasherServicer(hash_pb2_grpc.HasherServicer):
     def HashMe(self, request_iterator, context):
         for request in request_iterator:
-            print(f'Received request of size {len(request.data)} bytes.')
+            logging.info(f'Received request of size {len(request.data)} bytes.')
             sha1_hash = hashlib.sha1(request.data).digest()
+            logging.info(f'Sending response with hash {sha1_hash.hex()}.')
             yield hash_pb2.HashResponse(hash=sha1_hash)
 
 def serve():
