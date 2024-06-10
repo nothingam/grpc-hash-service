7e217e6168a76ef240d40e79ac162b742d3b8f86
Peter Olah <p.nothingam@gmail.com>
2024-06-10 10:04:52 +0200

    separate certificate used by the client

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ def read_file(path):
     with open(path, 'rb') as f:
         return f.read()
 
-SERVER_CERT = read_file('./certs/server.crt') 
+SERVER_CERT = read_file('./certs/server.crt')
+FE_SERVER_CERT = read_file('./certs/fe-server.crt')
 SERVER_KEY = read_file('./certs/server.key')
