03e41bc87810f718cf150ef5adb591b4acba0177
Peter Olah <p.nothingam@gmail.com>
2024-07-19 12:24:48 +0200

    adds CLI options to control the client

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import hash_pb2
 import hash_pb2_grpc
 import os
 import random
+import argparse
 
 from config import FE_SERVER_CERT
 
@@ -11,12 +12,19 @@ def generate_requests(num_requests, size):
         data = os.urandom(size)
         yield hash_pb2.HashRequest(data=data)
 
-def run():
+def run(num_requests, size):
+    print("Sending %d requests of size %d bytes" % (num_requests, size))
     with grpc.secure_channel('pepe.chimeratool.com:443', grpc.ssl_channel_credentials(root_certificates=FE_SERVER_CERT)) as channel:
         stub = hash_pb2_grpc.HasherStub(channel)
-        responses = stub.HashMe(generate_requests(100, 1024*1024))
+        responses = stub.HashMe(generate_requests(num_requests, size))
         for response in responses:
             print(f"Received hash: {response.hash.hex()}")
 
 if __name__ == '__main__':
-    run()
+    args = argparse.ArgumentParser(description='Client for the hash service')
+    args.add_argument('--num_requests', type=int, default=100, help='Number of requests to send')
+    args.add_argument('--size', type=int, default=1024*1024, help='Size of each request in bytes')
+
+    args = args.parse_args()
+
+    run(args.num_requests, args.size)
