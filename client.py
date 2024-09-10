3de0b89dc2dfc34c0c96c4932544793906479152
Peter Olah <p.nothingam@gmail.com>
2024-09-10 15:59:28 +0200

    improved traceability

--- client.py
+++ client.py
@@ -1,24 +1,26 @@
 import grpc
 import hash_pb2
 import hash_pb2_grpc
-import os
-import random
 import argparse
+import struct
 
 from config import FE_SERVER_CERT
 
 def generate_requests(num_requests, size):
-    for _ in range(num_requests):
-        data = os.urandom(size)
+    for idx in range(num_requests):
+        print(f"Generating request {idx}")
+        byte_data = idx.to_bytes(4, 'little')
+        data = byte_data * (size // 4)
         yield hash_pb2.HashRequest(data=data)
 
 def run(num_requests, size):
     print("Sending %d requests of size %d bytes" % (num_requests, size))
-    with grpc.secure_channel('pepe.chimeratool.com:443', grpc.ssl_channel_credentials(root_certificates=FE_SERVER_CERT)) as channel:
+    with grpc.secure_channel('pepe2.chimeratool.com:443', grpc.tls_channel_credentials(root_certificates=FE_SERVER_CERT, key_log_file_path=".client-key.log")) as channel:
         stub = hash_pb2_grpc.HasherStub(channel)
         responses = stub.HashMe(generate_requests(num_requests, size))
         for response in responses:
-            print(f"Received hash: {response.hash.hex()}")
+            idx = int.from_bytes(response.hash, 'little')
+            print(f"Received idx: {idx}")
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='Client for the hash service')
@@ -26,5 +28,9 @@ if __name__ == '__main__':
     args.add_argument('--size', type=int, default=1024*1024, help='Size of each request in bytes')
 
     args = args.parse_args()
+
+    if (args.size % 4) != 0:
+        print("Size must be a multiple of 4")
+        exit(1)
 
     run(args.num_requests, args.size)
--- server.py
+++ server.py
@@ -4,18 +4,19 @@ import hashlib
 import hash_pb2
 import hash_pb2_grpc
 from config import SERVER_CERT, SERVER_KEY
+import struct
 
 class HasherServicer(hash_pb2_grpc.HasherServicer):
     def HashMe(self, request_iterator, context):
         for request in request_iterator:
-            print(f'Received request of size {len(request.data)} bytes.')
-            sha1_hash = hashlib.sha1(request.data).digest()
-            yield hash_pb2.HashResponse(hash=sha1_hash)
+            first_word = struct.unpack('<I', request.data[0:4])[0]
+            print(f'Received idx: {first_word}, sending response')
+            yield hash_pb2.HashResponse(hash=first_word.to_bytes(4, 'little'))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hash_pb2_grpc.add_HasherServicer_to_server(HasherServicer(), server)
-    server.add_secure_port('[::]:50052', grpc.tls_server_credentials(SERVER_KEY, SERVER_CERT, '.ssl-key.log'))
+    server.add_secure_port('[::]:50052', grpc.tls_server_credentials(SERVER_KEY, SERVER_CERT, '.server-key.log'))
     server.start()
     print(f'Server is up and running.')
     server.wait_for_termination()
