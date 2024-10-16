f282c01abcc4ac2643a66901e86a763eeb4557a2
Peter Olah <p.nothingam@gmail.com>
2024-10-16 12:39:16 +0200

    adds fine grain configuration for client.py

--- client.py
+++ client.py
@@ -2,9 +2,7 @@ import grpc
 import hash_pb2
 import hash_pb2_grpc
 import argparse
-import struct
 
-from config import FE_SERVER_CERT
 
 def generate_requests(num_requests, size):
     for idx in range(num_requests):
@@ -13,9 +11,9 @@ def generate_requests(num_requests, size):
         data = byte_data * (size // 4)
         yield hash_pb2.HashRequest(data=data)
 
-def run(num_requests, size):
+def run(num_requests, size, host, port, server_cert):
     print("Sending %d requests of size %d bytes" % (num_requests, size))
-    with grpc.secure_channel('pepe2.chimeratool.com:443', grpc.ssl_channel_credentials(root_certificates=FE_SERVER_CERT)) as channel:
+    with grpc.secure_channel(f'{host}:{port}', grpc.ssl_channel_credentials(root_certificates=server_cert)) as channel:
         stub = hash_pb2_grpc.HasherStub(channel)
         responses = stub.HashMe(generate_requests(num_requests, size))
         for response in responses:
@@ -24,13 +22,19 @@ def run(num_requests, size):
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='Client for the hash service')
+    args.add_argument('--host', type=str, default='pepe2.chimeratool.com', help='Host to connect to')
+    args.add_argument('--port', type=int, default=443, help='Host to connect to')
     args.add_argument('--num_requests', type=int, default=100, help='Number of requests to send')
     args.add_argument('--size', type=int, default=1024*1024, help='Size of each request in bytes')
+    args.add_argument('--server_cert', type=str, default='./certs/fe-server.crt', help='Server crt')
 
     args = args.parse_args()
+
+    with open(args.server_cert, 'rb') as f:
+        server_cert = f.read()
 
     if (args.size % 4) != 0:
         print("Size must be a multiple of 4")
         exit(1)
 
-    run(args.num_requests, args.size)
+    run(args.num_requests, args.size, args.host, args.port,  server_cert)
--- config.py
+++
@@ -1,7 +0,0 @@
-def read_file(path):
-    with open(path, 'rb') as f:
-        return f.read()
-
-SERVER_CERT = read_file('./certs/server.crt')
-FE_SERVER_CERT = read_file('./certs/fe-server.crt')
-SERVER_KEY = read_file('./certs/server.key')
