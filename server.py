5f3423ea82c8d25d882e79ed8fa511e977db013d
Peter Olah <p.nothingam@gmail.com>
2024-10-16 10:57:21 +0200

    cli args for server.py

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 import grpc
 from concurrent import futures
-import hashlib
 import hash_pb2
 import hash_pb2_grpc
-from config import SERVER_CERT, SERVER_KEY
 import struct
+import argparse
 
 class HasherServicer(hash_pb2_grpc.HasherServicer):
     def HashMe(self, request_iterator, context):
@@ -13,13 +12,27 @@ class HasherServicer(hash_pb2_grpc.HasherServicer):
             print(f'Received idx: {first_word}, sending response')
             yield hash_pb2.HashResponse(hash=first_word.to_bytes(4, 'little'))
 
-def serve():
+def serve(port, server_key, server_cert):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hash_pb2_grpc.add_HasherServicer_to_server(HasherServicer(), server)
-    server.add_secure_port('[::]:50052', grpc.ssl_server_credentials([(SERVER_KEY, SERVER_CERT)]))
+    server.add_secure_port(f'[::]:{port}', grpc.ssl_server_credentials([(server_key, server_cert)]))
     server.start()
     print(f'Server is up and running.')
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    serve()
+    args = argparse.ArgumentParser(description='Server for the hash service')
+    args.add_argument('--port', type=int, default=50052, help='Port to listen on')
+    args.add_argument('--server_key', type=str, default='./certs/server.key', help='Server key')
+    args.add_argument('--server_cert', type=str, default='./certs/server.crt', help='Server crt')
+
+    args = args.parse_args()
+
+    def read_file(path):
+        with open(path, 'rb') as f:
+            return f.read()
+
+    server_key =  read_file(args.server_key)
+    server_cert = read_file(args.server_cert)
+
+    serve(args.port, server_key, server_cert)
